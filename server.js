9b9f1ddb59d75540c3fb6a89cc56ec51a527dc7d
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-15 19:10:26 +0100

    Update server.js
    Reset server settings to light config.

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 // SERVER START
-import 'dotenv/config';
-import app, { verifyDbConnection } from './app.js';
+import app from "./app.js";
 
 const port = process.env.PORT || 3000;
 
-(async () => {
-  // fail fast if DB isn’t reachable from this service
-  await verifyDbConnection();
-  app.listen(port, () => console.log(`Running on port: ${port}`));
-})().catch((err) => {
-  console.error('Startup failed:', err);
-  process.exit(1);
+app.listen(port, () => {
+  console.log("Running on port: " + port);
 });
