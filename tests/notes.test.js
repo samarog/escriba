2750b394c7315dab9192a5b5ab3f5e9ba2035b6a
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-16 02:46:34 +0100

    Return to dynamic Jest tests

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,5 +1,16 @@
-import request from "supertest";
-import app from "../app.js";
+import { jest } from '@jest/globals';
+
+jest.unstable_mockModule('pg', async () => {
+  const m = await import('./pg.mock.js');
+  return { __esModule: true, ...m, default: m.default };
+});
+
+const { __reset } = await import('pg');
+const { default: app } = await import('../app.js');
+
+import request from 'supertest';
+
+beforeEach(() => __reset());
 
 it('should return 200 and a Clear & LEave button', async  () => {
   const res = await request(app).get('/notes');
