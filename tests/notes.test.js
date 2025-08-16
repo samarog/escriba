0fbf6eea58d11bb56332f4b71da95ab6c8861f9e
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-16 01:53:23 +0100

    Update notes.test.js
    Rewrote sanitization methods for Jest tests.

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,8 +1,11 @@
 import { jest } from '@jest/globals';
 
-jest.unstable_mockModule('pg', () => import('./pg.mock.js'));
-
-const { __reset } = await import('./pg.mock.js');
+jest.unstable_mockModule('pg', async () => {
+  const m = await import('./pg.mock.js');
+  return m.default; // exports object from CJS
+});
+const { default: pgMock } = await import('./pg.mock.js');
+const { __reset } = pgMock;
 const { default: app } = await import('../app.js');
 
 import request from 'supertest';
