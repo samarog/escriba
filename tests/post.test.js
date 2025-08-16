ad22698dd63b95f4ca854e961c13c41d1ede8601
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-16 02:15:53 +0100

    Update post.test.js
    Fixed post.test design.

--- tests/post.test.js
+++ tests/post.test.js
@@ -4,18 +4,26 @@ jest.unstable_mockModule('pg', async () => {
   const m = await import('./pg.mock.js');
   return { __esModule: true, ...m, default: m.default };
 });
+
 const { __reset } = await import('pg');
 const { default: app } = await import('../app.js');
-
 import request from 'supertest';
 
 beforeEach(() => __reset());
 
 it('adds a note and then sees it in /notes', async () => {
-  const postRes = await request(app).post('/post').type('form').send({ notepost: 'Hello World' });
-  expect(postRes.statusCode).toBe(302);
+  const agent = request.agent(app);
+  const postRes = await agent
+    .post('/post')
+    .type('form')
+    .send({ notepost: 'Hello World' })
+    .redirects(1);
 
-  const notesRes = await request(app).get('/notes');
+  expect(postRes.statusCode).toBe(200);
+  expect(postRes.text).toContain('Hello World');
+
+  const notesRes = await agent.get('/notes');
+  
   expect(notesRes.statusCode).toBe(200);
   expect(notesRes.text).toContain('Hello World');
 });
