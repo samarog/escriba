7720b2cddb9128168b4619da74a9e640c9baa0bf
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-18 20:06:58 +0100

    Rewrite /health Jest test to accommodate db.ending connection.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,12 @@
 import request from "supertest";
-import app from "../app";
+import app, { closeDb } from "../app";
 
-test('GET /health returns 200', async () => {
-  const res = await request(app).get('/health');
+test("GET /health returns 200", async () => {
+  const res = await request(app).get("/health");
   expect(res.statusCode).toBe(200);
   expect(res.body).toEqual({ ok: true });
+});
+
+afterAll(async () => {
+  await closeDb(); // close pg.Client so Jest can exit
 });
